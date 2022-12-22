feb04f8d8ef3a98a9a9067ce738051b48c2f3107
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-22 22:04:45 +0300

    Задача №22

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i} | {Math.Pow(i, 2)}");
+    }
+}
